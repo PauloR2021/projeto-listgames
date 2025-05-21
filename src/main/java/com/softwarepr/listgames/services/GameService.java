7f15cf48209a1563adb413d9771056083ed9d778
PauloR2021 <paulotrindadeoficial@gmail.com>
2025-05-20 21:48:39 -0300

    Get game by Id - Implementando a classe de busca por Id

--- src/main/java/com/softwarepr/listgames/services/GameService.java
+++ src/main/java/com/softwarepr/listgames/services/GameService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.softwarepr.listgames.dto.GameDTO;
 import com.softwarepr.listgames.dto.GameMinDto;
 import com.softwarepr.listgames.entities.Game;
 import com.softwarepr.listgames.repositories.GameRepository;
+
+
 
 
 //Criando a Classe aonde vai retornar os valores do Banco de Dados através de Consultas
@@ -19,6 +23,13 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Game result = gameRepository.findById(id).get();
+        return new GameDTO(result);
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDto> findAll(){
         List<Game> result = gameRepository.findAll();
         return result.stream().map(GameMinDto::new).toList();
